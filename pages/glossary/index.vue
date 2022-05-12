abcc031ba5a61b11a4d8459fc6bb8562edec0052
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2022-05-12 22:33:20 +0400

    Initial commit

---
+++ pages/glossary/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <section v-if="glossary" class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Glossary</h1>
+      <posts post-type="glossary" :amount="10" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let glossary;
+    try {
+      glossary = await $content("glossary").fetch();
+      console.log('glossary', glossary)
+    } catch (e) {
+      error({ message: "Glossary not found" });
+    }
+    return { glossary };
+  },
+}
+</script>
