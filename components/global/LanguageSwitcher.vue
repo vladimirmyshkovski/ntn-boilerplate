d3e7a11975c1e446672c9be901b03c61026ad24e
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2022-05-13 22:07:53 +0400

    Update LanguageSwitcher component

--- components/global/LanguageSwitcher.vue
+++ components/global/LanguageSwitcher.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="fixed bottom-0 right-0 m-3">
-    <nuxt-link
-      v-if="$i18n.locale !== 'en'"
-      class="btn block"
-      :to="switchLocalePath('en')">
-        EN
-    </nuxt-link>
-    <nuxt-link
-      v-if="$i18n.locale !== 'ru'"
-      class="btn block"
-      :to="switchLocalePath('ru')"
-    >
-      RU
-    </nuxt-link>
+    <nuxt-link v-if="$i18n.locale !== 'en'" class="btn block" :to="switchLocalePath('en')"> EN </nuxt-link>
+    <nuxt-link v-if="$i18n.locale !== 'ru'" class="btn block" :to="switchLocalePath('ru')"> RU </nuxt-link>
   </div>
 </template>
